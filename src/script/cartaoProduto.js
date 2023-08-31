18db4409f49f1f893826b3b98d7a67e3769e6bb9
camagnum <magnumbenevides@gmail.com>
2023-08-30 23:50:30 -0300

    feat: added adding function at the cart

---
+++ src/script/cartaoProduto.js
@@ -0,0 +1,29 @@
+import { catalogo } from "./utilidades";
+import { adicionarAoCarrinho } from "./menuCarrinho";
+
+export function renderizarCatalogo() {
+	for (const produto of catalogo) {
+		const cartaoProduto = `<div id="card-produto-${produto.id}"
+		class="hover:cursor-pointer w-48 flex flex-col p-2 justify-between shadow-xl shadow-slate-400 rounded-lg group">
+        <img
+            src="./assets/img/${produto.nomeArquivoImagem}"
+            alt="Produto ${produto.id} do Magazine Hashtag"
+            class="group-hover:scale-110 duration-300 my-3 rounded-lg"
+        />
+        <p class="text.sm">${produto.marca}</p>
+        <p class="text.sm">${produto.nome}</p>
+        <p class="text.sm">$${produto.preco}</p>
+        <button id="adicionar-${produto.id}" class="bg-slate-950 text-slate-200 hover:scale-105 hover:bg-slate-700">
+            <i class="fa-solid fa-cart-plus"></i>
+        </button>
+    </div>`;
+
+		document.getElementById("container-produto").innerHTML += cartaoProduto;
+	}
+
+	for (const produto of catalogo) {
+		document
+			.getElementById(`adicionar-${produto.id}`)
+			.addEventListener("click",() => adicionarAoCarrinho(produto.id));
+	}
+}
--- src/script/menuCarrinho.js
+++ src/script/menuCarrinho.js
@@ -1,3 +1,5 @@
+import { catalogo } from "./utilidades";
+
 function abrirCarrinho() {
 	document.getElementById("carrinho").classList.add("right-[0px]");
 	document.getElementById("carrinho").classList.remove("right-[-360px]");
@@ -14,4 +16,33 @@ export function inicializarCarrinho() {
 
 	botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
 	botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
+}
+
+export function adicionarAoCarrinho(idProduto) {
+	const produto = catalogo.find((p) => p.id === idProduto);
+	const containerProdutosCarrinho =
+		document.getElementById("produtos-carrinho");
+	const cartaoProdutoCarrinho = `<article class="flex my-2 bg-slate-100 rounded-lg relative">
+		<button id="excluir-produto" class="absolute top-0 right-2">
+			<i	class="fa-solid fa-circle-xmark text-slate-900 hover:text-slate-700"></i>
+		</button>  
+		<img
+			src="./assets/img/product-${produto.id}.jpg"
+			alt="Carrinho: ${produto.nome}"
+			class="h-24 rounded-lg"
+		/>
+		<div class="py-3">
+			<p class="text-slate-900 text-sm font-semibold">
+				${produto.nome}
+			</p>
+			<p class="text-slate-400 text-xs">
+				Tamanho: M
+			</p>
+			<p class="text-green-700 text-lg">
+				$${produto.preco}
+			</p>
+		</div>
+	</article>
+	`;
+	containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
 }
